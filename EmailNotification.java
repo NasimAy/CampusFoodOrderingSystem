c45f6e7e09c9b28beddb94c4614ad421aac04d52
NasimAy <nassim.ayesh12345@gmail.com>
2026-05-14 22:46:29 +0300

    Update EmailNotification.java

--- EmailNotification.java
+++ EmailNotification.java
@@ -1,29 +1,9 @@
-public class CampusFoodOrderService {
-    public void placeOrder(String studentName, String foodName, String notificationType) {
-
+public class EmailNotification implements Notification {
+    @Override
+    public void send(String studentName, String foodName) {
         AppConfig config = AppConfig.getInstance();
-
-        System.out.println("Order created for: " + studentName);
-        System.out.println("Food item: " + foodName);
-        System.out.println("Delivery Fee: " + config.getDeliveryFee() + " TL");
-
-
-        Notification notification = NotificationFactory.createNotification(notificationType);
-
-        if (notification != null) {
-            notification.send(studentName, foodName);
-        } else {
-            System.out.println("Unknown notification type requested.");
-        }
-        System.out.println("-------------------------");
-    }
-
-    public static void main(String[] args) {
-        CampusFoodOrderService service = new CampusFoodOrderService();
-
-
-        service.placeOrder("Ali", "Chicken Sandwich", "EMAIL");
-        service.placeOrder("Zeynep", "Vegetarian Pizza", "SMS");
-        service.placeOrder("Omar", "Coffee", "PUSH");
+        System.out.println("Sending EMAIL notification...");
+        System.out.println("Dear " + studentName + ", your order for " + foodName +
+                " has been received by " + config.getUniversityName() + ".");
     }
 }
